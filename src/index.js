c2bef406d59cc435fdfcfa0092aea52e159f55dd
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-06-13 19:05:27 +0530

    feat: added the schedulers using node-cron

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from "./app.js"
 import dotenv from 'dotenv'
+import { startSourceScheduler } from "./schedulers/source-scheduler.js";
 const PORT = 4000 || process.env.PORT;
 
 dotenv.config({
@@ -10,3 +11,4 @@ app.listen(PORT, () => {
     console.log(`Server started at Port ${process.env.PORT}`);
 });
 
+startSourceScheduler()
---
+++ src/schedulers/source-scheduler.js
@@ -0,0 +1,14 @@
+import cron from "node-cron"
+import { fetchAllSources } from "../services/rss-parser.js"
+
+const EVERY_15_MIN = "*/15 * * * *"
+
+export function startSourceScheduler() {
+    console.log("[Scheduler] Running initial fetch...")
+    fetchAllSources();
+
+    console.log("[Scheduler] Scheduling RSS fetch every 15 minutes...")
+    cron.schedule(EVERY_15_MIN, () => {
+        fetchAllSources();
+    })
+}
